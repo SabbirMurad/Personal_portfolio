50bc5d3fcc92aaab3ac38d1ebb4e493c4ff26512
Sabbir Hassan <sbbir0087@gmail.com>
2022-11-09 23:09:43 +0600

    added closing and opening option for nav icons for mobile devices

--- js/main.js
+++ js/main.js
@@ -9,3 +9,22 @@ navMenuBar.addEventListener('click',()=>{
   bar2.classList.toggle('bar_2_active');
   bar3.classList.toggle('bar_3_active');
 })
+
+
+
+let iconOption =document.querySelector('.nav_icon_option');
+let navIcons =document.querySelector('.nav_icons');
+let isIconActive=false;
+let closeIcon="<i class='bx bx-comment' ></i>";
+let activeIcon="<i class='bx bx-plus close' ></i>";
+iconOption.addEventListener('click',()=>{
+  navIcons.classList.toggle('icon_active');
+  if(isIconActive){
+    iconOption.innerHTML=closeIcon;
+    isIconActive=false;
+  }
+  else{
+    iconOption.innerHTML=activeIcon;
+    isIconActive=true;
+  }
+})
