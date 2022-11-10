1deb6c5226d63765976da655b83cdbf2eeb49cd8
Sabbir Hassan <sbbir0087@gmail.com>
2022-11-10 12:51:00 +0600

    made all the icons available from local storage

--- js/main.js
+++ js/main.js
@@ -15,8 +15,12 @@ navMenuBar.addEventListener('click',()=>{
 let iconOption =document.querySelector('.nav_icon_option');
 let navIcons =document.querySelector('.nav_icons');
 let isIconActive=false;
-let closeIcon="<i class='bx bx-comment' ></i>";
-let activeIcon="<i class='bx bx-plus close' ></i>";
+let closeIcon=`
+<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M20 2H4c-1.103 0-2 .897-2 2v18l5.333-4H20c1.103 0 2-.897 2-2V4c0-1.103-.897-2-2-2zm0 14H6.667L4 18V4h16v12z"/></svg>
+`;
+let activeIcon=`
+<svg class="close" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M19 11h-6V5h-2v6H5v2h6v6h2v-6h6z"/></svg>
+`;
 iconOption.addEventListener('click',()=>{
   navIcons.classList.toggle('icon_active');
   if(isIconActive){
